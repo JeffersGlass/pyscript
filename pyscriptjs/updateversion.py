20d20980802fa149eccf3c79fa78cc5fd4e039f8
Jeff Glass <glass.jeffrey@gmail.com>
2022-10-05 12:04:33 -0500

    Don't overwrite extraneous version data

--- pyscriptjs/updateversion.py
+++ pyscriptjs/updateversion.py
@@ -7,14 +7,23 @@ import sys
 
 def update_version_info() -> None:
 
+    root_folder = [
+        p for p in pathlib.Path(os.getcwd()).parents if p.name == "pyscript"
+    ][0]
+    version_file = root_folder / "pyscriptjs" / "src" / "version_info.json"
+
+    try:
+        with open(version_file, "r") as fp:
+            version_data = json.load(fp)
+    except FileNotFoundError:
+        version_data = {}
+
     # Get release level from script argument
     release_level = sys.argv[1]
 
     # Get git tag info from script argument
     git_data = re.split(r"[\.-]", sys.argv[2])
     print(f"{git_data= }")
-
-    version_data = {}
 
     version_data["major"] = int(git_data[0])
     version_data["minor"] = int(git_data[1])
@@ -26,10 +35,7 @@ def update_version_info() -> None:
     root_folder = [
         p for p in pathlib.Path(os.getcwd()).parents if p.name == "pyscript"
     ][0]
-    # print(root_folder)
-    # print(os.listdir(root_folder))
     version_file = root_folder / "pyscriptjs" / "src" / "version_info.json"
-    # print(version_file)
 
     with open(version_file, "w") as fp:
         json.dump(version_data, fp, indent=2)
