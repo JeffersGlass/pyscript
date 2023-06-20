8a0e5da118332058d05a02c880a3f61deceee1d9
Jeff Glass <glass.jeffrey@gmail.com>
2023-06-20 15:55:55 -0500

    Partial removal

--- pyscriptjs/src/python/pyscript/_event_handling.py
+++ pyscriptjs/src/python/pyscript/_event_handling.py
@@ -3,7 +3,9 @@ import inspect
 import js
 from pyodide.ffi.wrappers import add_event_listener, remove_event_listener
 from functools import partial
+from collections import namedtuple
 
+EventPair = namedtuple("EventPair", ('event_type', 'element'))
 
 def when(event_type=None, selector=None):
     """
@@ -17,11 +19,11 @@ def when(event_type=None, selector=None):
 
         # Set to track JS elements
         if not hasattr(func, "_when_elements"): func._when_elements = list()
-        func._when_elements.extend(el for el in elements if el not in func._when_elements)
+        func._when_elements.extend(EventPair(event_type, el) for pair in elements if pair not in func._when_elements)
 
         # Public property 'when_elements' for acessing remaining elements on page
         if not hasattr(func, "when_elements"): func.when_elements = property(
-                fget=lambda self: [el for el in self._when_elements if js.document.contains(el)],
+                fget=lambda self: [pair for pair in self._when_elements if js.document.contains(pair.element)],
                 )
             
         func.remove_when = partial(_remove_when, func)
@@ -41,13 +43,17 @@ def when(event_type=None, selector=None):
 
     return decorator
 
-def _remove_when(func, event, selector=None):
-    if selector:
-        all_matches = js.document.querySelectorAll(selector)
-        for el in (el for el in func._when_elements if el in all_matches):
-            remove_event_listener(el, event, func)
-        func._when_elements = [el for el in func._when_elements if not el in js.document.querySelectorAll(selector)]
-    else:
-        for el in func._when_elements:
-            remove_event_listener(el, event, func)
-        func._when_elements = list()
+def _remove_when(func, *, event=None, selector=None):
+    if event:
+        if selector:
+            all_matches = js.document.querySelectorAll(selector)
+            for pair in (pair for pair in func._when_elements if pair.element in all_matches):
+                remove_event_listener(pair.element, pair.event_type, func)
+            func._when_elements = [pair for pair in func._when_elements if not pair.element in all_matches]
+        else: # Remove all matched events
+            for pair in (pair for pair in func._when_elements if pair.event_type == event):
+                remove_event_listener(pair.element, event, func)
+            func._when_elements = list()
+    else: # No event provided
+        # TODO this part
+        pass
