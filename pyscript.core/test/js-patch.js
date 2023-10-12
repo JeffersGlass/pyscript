9673649a3e581fdf62773bd6f3a02e7fb026bc7f
Jeff Glass <glass.jeffrey@gmail.com>
2023-10-12 11:00:50 -0500

    First

---
+++ pyscript.core/test/js-patch.js
@@ -0,0 +1,27 @@
+import { hooks } from "@pyscript/core";
+
+hooks.codeBeforeRunWorker.add(`
+    print("Starting codeBeforeRunWorker")
+    import pyscript
+    import js
+    import sys
+
+    class Wrapper(object):
+        def __init__(self, obj):
+            setattr(self, "_wrapped_obj", obj)
+        def __getattribute__(self, attr):
+            print(attr)
+            if attr == "document": return pyscript.document
+            print(f"About to try to lookup {attr}")
+
+            # Equivalent to return 
+            #return object.__getattribute__(self._wrapped_obj, attr)
+
+    sys.modules['js'] = Wrapper(js)
+    print("js is wrapped?")
+    `
+)
+
+hooks.codeAfterRunWorker.add(`
+sys.modules['js'] = js
+`)
